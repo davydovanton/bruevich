cc327132b50310d394a199652ee9d04198bb1502
Anton Davydov <antondavydov.o@gmail.com>
2016-07-26 23:29:53 +0300

    Create simple version of benchmark

--- lib/bruevich.rb
+++ lib/bruevich.rb
@@ -1,5 +1,50 @@
 require "bruevich/version"
 
-module Bruevich
-  # Your code goes here...
+class Bruevich
+  ITERATIONS = [1, 10, 50, 100, 150]
+
+  attr_reader :result
+
+  def initialize
+    @result = {}
+  end
+
+  def bench
+    ITERATIONS.each do |count|
+      result[count] = {}
+      result[count][:time] = {}
+      result[count][:time][:per_iteration] = []
+
+      result[count][:mem] = {}
+      result[count][:mem][:per_iteration] = []
+
+      GC.disable
+      GC.start
+      mem_start = memory
+      time_start = Time.now
+
+      count.times do
+        yield
+      end
+
+      result[count][:time][:total] = Time.now - time_start
+      result[count][:mem][:total] = memory - mem_start
+      GC.enable
+    end
+
+    calculate
+  end
+
+private
+
+  def calculate
+    result.each do |count, _|
+      result[count][:time][:average] = result[count][:time][:total] / count
+      result[count][:mem][:average] = result[count][:mem][:total] / count
+    end
+  end
+
+  def memory
+    `ps ax -o pid,rss | grep -E "^[[:space:]]*#{$$}"`.strip.split.map(&:to_i).last
+  end
 end
--- lib/bruevich/version.rb
+++ lib/bruevich/version.rb
@@ -1,3 +1,3 @@
-module Bruevich
+class Bruevich
   VERSION = "0.1.0"
 end
